7481e36bcaceccf256d2fc270ee2e9730b2cfdb1
Puzzles <127057721+satoshipuzzles@users.noreply.github.com>
2024-11-25 15:29:42 -0800

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from '../styles/GlobalStyle';
+
+const theme = {
+  colors: {
+    background: '#121212',
+    text: '#ffffff',
+    primary: '#bb86fc',
+    secondary: '#03dac6',
+  },
+};
+
+export default function App({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
