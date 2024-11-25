129cd6fb35324328d8420afff40fdbc73722f9e9
Puzzles <127057721+satoshipuzzles@users.noreply.github.com>
2024-11-25 15:30:21 -0800

    Create GlobalStyle.js

---
+++ styles/GlobalStyle.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    background-color: #121212;
+    color: #ffffff;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
+
+export default GlobalStyle;
